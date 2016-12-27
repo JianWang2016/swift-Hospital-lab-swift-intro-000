6c0eeb543deaef9f3627d76ce95ea40bbc5a37a5
Jian Wang <adena@Jians-MacBook-Pro.local>
2016-12-27 17:55:56 -0500

    Completed problems

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -4,12 +4,44 @@
 
 
 
+struct AdvisoryBoardMember {
+    var name: String
+    var vacationDays = 30
+    
+    func wages() -> Double {
+        return 100_000.00
+    }
+}
+
+extension AdvisoryBoardMember: Employee {
+    
+}
 
 
+struct Principal {
+    var name: String
+    var vacationDays = 20
+    
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
+
+extension Principal: Employee, Reprimand {
+    
+}
 
 
+struct Teacher {
+    var name: String
+    var vacationDays = 20
+    
+//    func wages() -> Double {
+//        return 50_000.00
+//    }
+}
 
-
-
-
+extension Teacher: Employee, Teach {
+    
+}
 
